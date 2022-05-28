6647964b573ae8665033544b5ae9b329aa5bf3bc
42vio <viokeo@gmail.com>
2022-05-29 00:13:09 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generat
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
 
 #3. Change default theme
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/g'feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
