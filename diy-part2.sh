b207cb0218cf81967517dbaf0244656e888fb2e9
42vio <viokeo@gmail.com>
2022-05-28 23:54:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,11 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.2.5/g' package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' package/lean/default-settings/files/zzz-default-settings
+
+#3. Change default theme
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g'feeds/luci/collections/luci/Makefile
